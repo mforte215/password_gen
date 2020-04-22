46a0aabe0ef97e1682f7b80f54dbd24924fb67ae
MForte101 <mark.fortee@gmail.com>
2020-04-22 13:30:20 -0400

    first commit, mostly complete password app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pwgen';
+
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  passwordLength = 0;
+
+
+  onButtonCLick() {
+
+
+    const numbers = '0123456789';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()-';
+    let validChars = '';
+
+    if(this.includeLetters)
+    {
+      validChars += letters;
+    }
+    if(this.includeNumbers)
+    {
+      validChars += numbers;
+    }
+    if(this.includeSymbols)
+    {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for(let i = 0; i < this.passwordLength; i++)
+    {
+
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+
+    }
+
+
+   this.password = generatedPassword;
+
+
+  }
+
+  onChangeUseLetters()
+  {
+
+    this.includeLetters = !this.includeLetters;
+
+  }
+
+  onChangeUseNumbers()
+  {
+
+    this.includeNumbers = !this.includeNumbers;
+
+  }
+
+  onChangeUseSymbols()
+  {
+
+    this.includeSymbols = !this.includeSymbols;
+
+  }
+
+  onChangeLength(lengthOfPassword : string)
+  {
+
+    const parsedValue = parseInt(lengthOfPassword);
+    if(!isNaN(parsedValue))
+    {
+      this.passwordLength = parsedValue;
+    }
+    
+  }
+
+
 }
